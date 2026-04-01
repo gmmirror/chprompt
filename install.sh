470f3a3a6c53b891259956943c3b791d12434ddd
gmmirror <blackstar76776@gmail.com>
2026-04-01 21:09:01 +0700

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-command git clone --depth 1 \
-    'https://github.com/Zeronetsec/Chprompt.git' \
-    "${PREFIX}/tmp/chprompt"
-command chmod +x -Rv "${PREFIX}/tmp/chprompt"
-command bash "${PREFIX}/tmp/chprompt"
 [[ -d "${PREFIX}/tmp/chprompt" ]] && {
     command rm -rfv "${PREFIX}/tmp/chprompt"
 }
 
+command git clone --depth 1 \
+    'https://github.com/Zeronetsec/Chprompt' \
+    "${PREFIX}/tmp/chprompt"
+command chmod +x -R "${PREFIX}/tmp/chprompt"
+command bash "${PREFIX}/tmp/chprompt"
 exit 0
