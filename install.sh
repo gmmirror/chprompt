02240e8f77ea2c4f977f61d875a0b85e3d96c2d3
gmmirror <blackstar76776@gmail.com>
2026-04-02 05:36:37 +0700

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-[[ -d "${PREFIX}/tmp/chprompt" ]] && {
+if [[ ! -d "${PREFIX}/tmp" ]]; then
+    command mkdir -p "${PREFIX}/tmp"
+fi
+
+if [[ -d "${PREFIX}/tmp/chprompt" ]]; then
     command rm -rf "${PREFIX}/tmp/chprompt"
-}
+fi
 
 command git clone --depth 1 \
     'https://github.com/Zeronetsec/Chprompt' \
@@ -12,5 +16,3 @@ cd "${PREFIX}/tmp/chprompt" || exit 1
 command chmod +x -R "install.sh"
 command bash "install.sh"
 cd
-
-exit 0
